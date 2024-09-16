a19fd6f39bff970dbd8b14f4a7ce4b1d63e8f3a6
Dehv <71988020+deboragoncalves1@users.noreply.github.com>
2024-09-16 17:02:00 -0300

    Add files via upload

---
+++ Migrations/20240506175212_AjustandoNomeProp.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoEmprestimoLivros.Migrations
+{
+    public partial class AjustandoNomeProp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NomeCOmpleto",
+                table: "Usuarios",
+                newName: "NomeCompleto");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NomeCompleto",
+                table: "Usuarios",
+                newName: "NomeCOmpleto");
+        }
+    }
+}
